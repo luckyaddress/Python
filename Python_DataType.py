3ebd0ba3405dd3db73deb30880bf9a7a6eff7803
ykk <ykk@c225.pharm.kmu.edu.tw>
2017-02-17 15:06:35 +0800

    commit.

--- Python_DataType.py
+++ Python_DataType.py
@@ -5,3 +5,26 @@ print(0.025.is_integer())
 
 # 跳脫字元\t --tab鍵
 print('Hello\tJack!')
+# 列印單引號或者雙引號 使用\'或\"
+print('\'Hello Jack\'')
+print('\"測試\"')
+
+# 設定變數為布林值
+a = True
+b = False
+print(a,b)
+
+# 資料陣列
+a = []
+b = [1, 2, "3",[4],(5)]
+print(b[3])
+
+# 運算子
+a = True
+b = False
+c = a and b
+print(c)
+x = a or b
+print(x)
+d = not b
+print(d)
