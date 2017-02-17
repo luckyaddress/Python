84422f67efbf94258020c90e1b7c10628e425c33
ykk <ykk@c225.pharm.kmu.edu.tw>
2017-02-17 17:12:01 +0800

    commit.

--- Python_DataType.py
+++ Python_DataType.py
@@ -28,3 +28,39 @@ x = a or b
 print(x)
 d = not b
 print(d)
+
+# 算術運算子
+a = 5
+b = a + 3
+print(b)
+b = a * 3
+print(b)
+b = a/5
+print(b)
+b = a - 2
+print(b)
+b = a % 2 
+print(b)
+b = a ** 2
+print(b)
+
+# 函數使用return
+def test1(a,b):
+    if a > b:
+        return "好"
+    else:
+        return "不好"
+
+print(test1(5,8)) # 列印函數的回傳值！
+
+# 類別
+class Cat:
+    def voice(self, a, b):
+        self.a = a
+        self.b = b
+        # voice方法先把 a跟b指定給這個類別的本身self
+    def meow(self):
+        return self.a + self.b
+d = Cat()
+d.voice(22,33)
+print(d.meow())
