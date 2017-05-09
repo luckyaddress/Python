63946b80a8468f7cd0e6fe9176b0e60bc149e135
ykk <ykk@c225.pharm.kmu.edu.tw>
2017-05-09 17:26:22 +0800

    commit

--- class_function.py
+++ class_function.py
@@ -3,7 +3,7 @@
 
 def Test1():
     print('Earn Money')
-    print('Buy Stock')
+    print('Buy iMac')
 Test1()
 
 def Test2(x):
@@ -15,29 +15,38 @@ class Cat:
     def meow(self):
         # self指向當這個類別被new起來時的那個物件
         # 只是這樣使用，每次new一個Cat物件，呼叫一次方法，就會重複創建這個方法
-        print('Fuck!') 
+        print('測試類別') 
 a = Cat()
 a.meow()
 
+#靜態方法，跟@classmethod方法類似，可以new一個物件或者不new一個物件，
+#方法內有多少參數，new出來的物件，就有多少參數。
 class dog:
     @staticmethod
     def wow(x,y):
-        print('wow'* x * y)
+        print('wow '* x * y)
         # static靜態方法，跟C#宣告物件後使用的方法較為雷同
 
-a = dog()
+a = dog() #相當於new一個物件，將其實體化
 a.wow(2,3)
+
+#staticmethod，亦可不new物件執行
+dog.wow(4,1)
 
 #類別方法 
 #不需要宣告一個物件，宣告類別，建立方法後即可使用！
+#要new一個物件亦可
 class pig:
-    @classmethod
-    def hoho(clz,x, y):
-        print('success', x ,y)
+    
+    var1 = "Try classmethod"
+    @classmethod      
+    def Print3(paraTest):  
+        print "print 3: ", paraTest.var1  
 
-pig.hoho(10,20)
+pig.Print3()
 
 # 函數使用return
+
 def test1(a,b):
     if a > b:
         return "好"
@@ -49,11 +58,12 @@ print(test1(5,8)) # 列印函數的回傳值！
 # 類別
 class Cat:
     def voice(self, a, b):
-        self.a = a
-        self.b = b
-        # voice方法先把 a跟b指定給這個類別的本身self
+        self.num1 = a
+        self.num2 = b
+        # voice方法先把 a跟b 指定給這個類別的本身self
     def meow(self):
-        return self.a + self.b
-d = Cat()
-d.voice(22,33)
-print(d.meow())
+        return self.num1 + self.num2
+
+d = Cat()  # new一個Cat物件
+d.voice(22,33)  # Cat物件下的voice方法，會將Cat.num1跟Cat.num2這兩個屬性 設定為a跟b兩個參數
+print(d.meow()) # 之後meow()方法用到時，num1跟num2即會是a跟b的值 
