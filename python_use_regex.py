fe6f25a8b4efbc65f310ec0193d2b9f25cfea7ae
luckyaddress <deathkao@gmail.ocm>
2017-05-11 04:59:44 +0800

    commit

---
+++ python_use_regex.py
@@ -0,0 +1,10 @@
+#-*- coding: utf-8 -*- 
+# 使用中文註解，要加上上面那一行的編碼
+
+import re           #import re 作為引入regex函式庫
+pattern = '[abc]'   #設定正規式要搜尋的pattern
+string = 'aAbBcC'   #搜尋字串
+match_string = re.findall(pattern, string)   #設定match_string為re函式庫下findall方法的回傳字串
+# 用法findall(第一字串為搜尋的pattern, 第二個為欲待分析的字串)
+x = match_string
+print  x
