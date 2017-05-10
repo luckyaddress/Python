88c3b6f9844f6f7e4f99158ac83351ecc31a05c8
luckyaddress <deathkao@gmail.ocm>
2017-05-11 05:43:54 +0800

    commit

--- python_use_regex.py
+++ python_use_regex.py
@@ -7,4 +7,4 @@ string = 'aAbBcC'   #搜尋字串
 match_string = re.findall(pattern, string)   #設定match_string為re函式庫下findall方法的回傳字串
 # 用法findall(第一字串為搜尋的pattern, 第二個為欲待分析的字串)
 x = match_string
-print  x
+print  x     # 輸出 ['a', 'b', 'c']
