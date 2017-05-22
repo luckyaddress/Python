10adf22775982b64b7e2ba092827709aa9156643
ykk <ykk@c225.pharm.kmu.edu.tw>
2017-05-22 17:26:10 +0800

    commit

--- loop_2.py
+++ loop_2.py
@@ -6,5 +6,13 @@
 for i in range(1,10,1):
     for j in range(1,10,1):
         print (str(i)+ "*" + str(j) + "=" + str(i*j))
-        
+
 # 第一層 i 每跑1次，j要跑9次，i總共要跑9次，所以可產生出99乘法表
+
+# 終止跟繼續迴圈 一樣使用break跟continue
+a = 0
+while True:
+    print(str(a))
+    a += 1
+    if a > 5:
+        break
