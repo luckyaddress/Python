ed0f737377f46683670daebccb579ca20f4aef25
ykk <ykk@c225.pharm.kmu.edu.tw>
2017-05-25 10:17:45 +0800

    commit

---
+++ module_class.py
@@ -0,0 +1,11 @@
+#-*- coding: utf-8 -*- 
+# 使用中文註解，要加上上面那一行的編碼
+
+# 如果有多個模組要匯入，使用,號隔開
+import module_example  # 匯入模組後，下方即可使用該模組的方法或變數
+
+x = module_example.bmi_cal(1.79, 0.95)
+
+print(x)
+
+# Python亦可匯入內建模組,並使用別名來呼叫模組
---
+++ module_example.py
@@ -0,0 +1,7 @@
+#-*- coding: utf-8 -*- 
+# 使用中文註解，要加上上面那一行的編碼
+# 外部撰寫一個function 計算bmi，並且後續當作一個模組import到module_class.py
+
+def bmi_cal(h,w):
+    r = w / h /h
+    return r
