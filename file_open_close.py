1ccbc679aa2c9a24e5bca2f39499455a0213868a
ykk <ykk@c225.pharm.kmu.edu.tw>
2017-05-31 14:20:50 +0800

    commit

---
+++ file_open_close.py
@@ -0,0 +1,8 @@
+#-*- coding: utf-8 -*- 
+# 使用中文註解，要加上上面那一行的編碼
+python_file = open("Python_test.txt", "a")
+# 同一個資料夾下的路徑，不用加上\
+python_file.write("測試加入第一行\n")
+python_file.write("測試加入第二行\n")
+
+python_file.close()
