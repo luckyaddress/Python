db085679bd952eb7601632c165568db88c02f0cc
ykk <ykk@c225.pharm.kmu.edu.tw>
2017-06-02 09:50:18 +0800

    commit

--- exceptIon_deal.py
+++ exceptIon_deal.py
@@ -1,2 +1,10 @@
 #-*- coding: utf-8 -*- 
 # 使用中文註解，要加上上面那一行的編碼
+
+# 例外處理: try / except區塊
+try:
+    try_open = open("Exception.txt", "r")
+    print(try_open.read())
+    try_open.close()
+except:
+    print("錯誤，檔案不存在！")
