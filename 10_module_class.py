e6d8335f8cb1b4698caf44f972dd9ebcb3fe7c57
ykk <ykk@c225.pharm.kmu.edu.tw>
2017-06-07 09:06:41 +0800

    commit

--- 10_module_class.py
+++ 10_module_class.py
@@ -2,9 +2,9 @@
 # 使用中文註解，要加上上面那一行的編碼
 
 # 如果有多個模組要匯入，使用,號隔開
-import module_example  # 匯入模組後，下方即可使用該模組的方法或變數
+import import_10_module_example  # 匯入模組後，下方即可使用該模組的方法或變數
 
-x = module_example.bmi_cal(1.79, 0.95)
+x = import_10_module_example.bmi_cal(1.79, 0.95)
 
 print(x)
 
@@ -16,10 +16,10 @@ rn_2 = R.randint(20, 60)
 print(random_n, rn_2)
 
 # 僅匯入模組的部份
-from module_example_2 import print_name
+from import_10_module_example_2 import print_name
 # import module_example_2 上面為限制模組的部份，僅使用print_name的功能
 # print(module_example_2.gender)
-module_example.print_name("Wakaki")
+import_10_module_example.print_name("Wakaki")
 
 # 物件導向 物件與類別
 # 定義類別
